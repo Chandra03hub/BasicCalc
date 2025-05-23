346145e5207f6cddb067c6b0f78b68a0110c5bbd
Chandrasekaran Ramachandran <55437766+Chandra03hub@users.noreply.github.com>
2025-05-23 20:05:05 +0530

    Add files via upload

---
+++ MyPackage/__init__.py
@@ -0,0 +1,5 @@
+#from .addition import add     # from MyPackage import add 
+#from .subtraction import subtract
+#from .multiplication import multiply
+#from .division import divide
+#__all__ = ['add', 'subtract', 'multiply', 'divide']   # -> from MyPackage import *
